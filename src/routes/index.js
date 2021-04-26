43524350dbc5b41d169628012e9668781b6878cc
OrangeJuice <rgullaksen@gmail.com>
2021-04-26 21:47:16 +0200

    init react native and folder structure 🙌

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text> Init </Text>
+    </View>
+  );
+}
+
+export default App;
